fcbb1818ed525f64e67306c0c3d076d374013223
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-05-19 14:27:18 +0700

    LoveCTF

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-import { useEffect } from "react";
 import "./App.css";
-import data from "./data";
+import { target_word,target_number, list_number } from './hiddendata';
 
 const App = () => {
 
-  const character_1 = [0, 1, 2, 20, 40, 60, 80, 81, 82];
-  const character_2 = [4, 7, 24, 27, 44, 45, 46, 47, 64, 67, 84, 87];
-  const character_3 = [9, 10, 11, 12, 29, 32, 49, 52, 69, 72, 89, 90, 91, 92];
-  const character_4 = [14, 18, 34, 35, 38, 54, 56, 58, 74, 77, 78, 94, 98];
-
-  const target_word = [
-    ...character_1,
-    ...character_2,
-    ...character_3,
-    ...character_4,
-  ];
-
-  useEffect(() => {
-    if (document.getElementsByClassName("game")) {
-      console.log(document.getElementsByClassName("game"));
-    }
-  }, []);
-
   const BoardWord = () => {
-    return [...Array(100)].map((_, index) => {
+    return list_number.map((item, index) => {
       if (target_word.includes(index)) {
-
-        return <div className="c" key={index}>{index}</div>;
+        if (target_number.includes(index)) {
+        }
+        return <div className={target_number.includes(item) ? "c d" : "c c"} key={item}>{item}</div>;
       }
-
       if (index % 2) {
-        return <div className="b" key={index}>{index}</div>;
+        return <div className={target_number.includes(item) ? "b d" : "b b"} key={item}>{item}</div>;
       } else {
-        return <div className="a" key={index}>{index}</div>;
+        return <div className={target_number.includes(item) ? "a d" : "a a"} key={item}>{item}</div>;
       }
     });
   };
