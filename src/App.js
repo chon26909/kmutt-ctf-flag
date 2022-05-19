c095445d17da69d4caccf7b4022e867fb247202e
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-05-19 14:52:59 +0700

    Easy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import "./App.css";
-import { target_word,target_number, list_number } from './hiddendata';
+import { target_word, target_number, list_number } from "./hiddendata";
 
 const App = () => {
-
   const BoardWord = () => {
     return list_number.map((item, index) => {
       if (target_word.includes(index)) {
         if (target_number.includes(index)) {
         }
-        return <div className={target_number.includes(item) ? "c d" : "c c"} key={item}>{item}</div>;
+        return (
+          <div
+            className={target_number.includes(item) ? "a c" : "a a"}
+            key={item}
+          >
+            {item}
+          </div>
+        );
       }
-      if (index % 2) {
-        return <div className={target_number.includes(item) ? "b d" : "b b"} key={item}>{item}</div>;
-      } else {
-        return <div className={target_number.includes(item) ? "a d" : "a a"} key={item}>{item}</div>;
-      }
+
+      return (
+        <div
+          className={target_number.includes(item) ? "b c" : "b b"}
+          key={item}
+        >
+          {item}
+        </div>
+      );
+
+      // else {
+      //   return <div className={target_number.includes(item) ? "a d" : "a a"} key={item}>{item}</div>;
+      // }
     });
   };
 
